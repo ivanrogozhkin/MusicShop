9c6599b32daf34717ee827a4217b6b1d7297c287
xsavzh <drockick@gmail.com>
2023-01-07 22:09:10 +0400

    Update code

--- app/src/main/java/com/xsavzh/musicshop/OrderActivity.kt
+++ app/src/main/java/com/xsavzh/musicshop/OrderActivity.kt
@@ -6,15 +6,15 @@ import com.xsavzh.musicshop.databinding.ActivityOrderBinding
 
 class OrderActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOrderBinding
-    lateinit var userName: String
+    private lateinit var userName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOrderBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val receivedOrderIntent = intent
-        userName = receivedOrderIntent.getStringExtra("userName").toString()
+        userName = intent.getStringExtra("userName")!!
+
         binding.orderTextView.text = userName
     }
 }
